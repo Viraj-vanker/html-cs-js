ad839f5e1cc5960f8847a810ced04c12c4d5ed19
viraj vanker... <natvarchampa1950@gmail.com>
2025-05-08 10:24:20 +0530

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+var a=document.querySelector('.box');
+var b=document.querySelector('.box1');
+
+function size(){
+    if(b.style.width === '100px'){
+        b.style.width= '200px';
+    }
+    else{
+        b.style.width= '100px';
+    }
+    
+}
